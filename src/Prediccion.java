8debd383b7b66ffb017f109461e577c9672e5b85
julenb <21701719@live.uem.es>
2018-11-02 20:24:52 +0100

    Corrección predicción

--- src/Prediccion.java
+++ src/Prediccion.java
@@ -3,14 +3,16 @@ import java.util.Date;
 public class Prediccion {
 
     private String nombreCiudad;
-    private int temp_min,temp_max,humedad;
-    private Date amanecer,ocaso;
+    private double temp_min, temp_max;
+    private long humedad;
+    private Date fecha, amanecer, ocaso;
 
-    public Prediccion(String nombreCiudad, int temp_min, int temp_max, int humedad, Date amanecer, Date ocaso) {
+    public Prediccion(String nombreCiudad, double temp_min, double temp_max, long humedad, Date fecha, Date amanecer, Date ocaso) {
         this.nombreCiudad = nombreCiudad;
         this.temp_min = temp_min;
         this.temp_max = temp_max;
         this.humedad = humedad;
+        this.fecha = fecha;
         this.amanecer = amanecer;
         this.ocaso = ocaso;
     }
@@ -19,15 +21,15 @@ public class Prediccion {
         return nombreCiudad;
     }
 
-    public int getTemp_min() {
+    public double getTemp_min() {
         return temp_min;
     }
 
-    public int getTemp_max() {
+    public double getTemp_max() {
         return temp_max;
     }
 
-    public int getHumedad() {
+    public long getHumedad() {
         return humedad;
     }
 
@@ -38,4 +40,9 @@ public class Prediccion {
     public Date getOcaso() {
         return ocaso;
     }
+
+    public Date getFecha() {
+        return fecha;
+    }
+    
 }
