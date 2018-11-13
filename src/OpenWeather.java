e8f9c2b06202ed5a26dfc1db1fee481735d923b3
julenb <21701719@live.uem.es>
2018-11-13 16:09:02 +0100

    Añadir icono, temperatura actual, generar fichero

--- src/OpenWeather.java
+++ src/OpenWeather.java
@@ -1,16 +1,21 @@
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class OpenWeather {
+
+    private final static double KELVIN = 273.15;
 
     private static String descargarJSON(String ciudad) throws IOException {
         String respuesta = "";
@@ -27,33 +32,53 @@ public class OpenWeather {
         JSONParser parser = new JSONParser();
         JSONObject rootObject = (JSONObject) parser.parse(json);
         String nombreCiudad = (String) rootObject.get("name");
-        double temp_min = (double) ((JSONObject) rootObject.get("main")).get("temp_min") - 273;
-        double temp_max = (double) ((JSONObject) rootObject.get("main")).get("temp_max") - 273;
+        String icono = (String) ((JSONObject) ((JSONArray) rootObject.get("weather")).get(0)).get("icon");
+        double tempActual = (double) ((JSONObject) rootObject.get("main")).get("temp") - KELVIN;
+        double temp_min = (double) ((JSONObject) rootObject.get("main")).get("temp_min") - KELVIN;
+        double temp_max = (double) ((JSONObject) rootObject.get("main")).get("temp_max") - KELVIN;
         long humedad = (long) ((JSONObject) rootObject.get("main")).get("humidity");
-        Date fecha = new Date((long) rootObject.get("dt")*1000);
-        Date amanecer = new Date((long) ((JSONObject) rootObject.get("sys")).get("sunrise")*1000);
-        Date ocaso = new Date((long) ((JSONObject) rootObject.get("sys")).get("sunset")*1000);
-        return new Prediccion(nombreCiudad, temp_min, temp_max, humedad, fecha, amanecer, ocaso);
+        Date fecha = new Date((long) rootObject.get("dt") * 1000);
+        Date amanecer = new Date((long) ((JSONObject) rootObject.get("sys")).get("sunrise") * 1000);
+        Date ocaso = new Date((long) ((JSONObject) rootObject.get("sys")).get("sunset") * 1000);
+        System.out.println((String) ((JSONObject) rootObject.get("sys")).get("country"));
+        return new Prediccion(nombreCiudad, icono, tempActual, temp_min, temp_max, humedad, fecha, amanecer, ocaso);
     }
 
     private static String generarHTML(Prediccion prediccion) {
+        String fechaFormateada = new SimpleDateFormat("dd/MM/YYYY").format(prediccion.getFecha());
+        SimpleDateFormat formateadorHora = new SimpleDateFormat("HH:mm");
         // language=HTML
         String html = "<html\n><head>\n<meta charset='UTF-8'>\n" +
                 "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\">\n" +
                 "</head>\n<body>\n" +
                 "<div style=\"text-align: center;\"><h1>" + prediccion.getNombreCiudad() + "</h1>\n" +
-                "<h2>" + prediccion.getFecha() + "</h2></div>\n" +
+                "<h2><img src=\"http://openweathermap.org/img/w/" + prediccion.getIcono() + ".png\" style='height: 100px; width: auto;'></h2>\n" +
+                "<h2>Temperatura actual: " + String.format("%.1f",prediccion.getTempActual()) + "</h2>" +
+                "<h2>" + fechaFormateada + "</h2></div>\n" +
                 "<table class='centered' style='margin: auto; width: 800px'>\n<tbody>\n" +
-                "<tr><td>Amanecer/Ocaso</td><td>T. Máxima</td><td>T. Mínima</td></tr>\n" +
-                "<tr><td>" + prediccion.getAmanecer() + " " + prediccion.getOcaso() + "</td>" +
-                "<td>" + prediccion.getTemp_max() + "</td><td>" + prediccion.getTemp_min() + "</td></tr>\n";
+                "<tr><td>Amanecer</td><td>Ocaso</td><td>T. Máxima</td><td>T. Mínima</td></tr>\n" +
+                "<tr><td>" + formateadorHora.format(prediccion.getAmanecer()) + "</td>" +
+                "<td>" + formateadorHora.format(prediccion.getOcaso()) + "</td>" +
+                "<td>" + String.format("%.1f",prediccion.getTempMax()) + "</td><td>" + String.format("%.1f",prediccion.getTempMin()) + "</td></tr>\n";
         // language=HTML
         html += "\n</tbody></table>\n</body>\n</html>\n";
         return html;
     }
 
+    private static void generarFichero(String htmlPagina) {
+        try {
+            FileWriter fw = new FileWriter("./prediccion.html", false);
+            fw.write(htmlPagina);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) throws IOException, ParseException {
-        System.out.println(parsearJSON(descargarJSON("Valencia")));
+        Scanner scanner = new Scanner(System.in);
+        generarFichero(generarHTML(parsearJSON(descargarJSON(scanner.nextLine()))));
     }
 
 }
